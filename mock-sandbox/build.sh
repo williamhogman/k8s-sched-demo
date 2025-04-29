c6e72fb4c7d0f6e9cbe241968d00ad506d05f42e
William Rudenmalm <me@whn.se>
2025-04-30 01:03:48 +0200

    fix mocksandbox build script

--- mock-sandbox/build.sh
+++ mock-sandbox/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DOCKER_HOST=unix:///Users/whn/.rd/docker.sock
+export DOCKER_HOST=$(docker context inspect | jq '.[0].Endpoints.docker.Host')
 export KO_DOCKER_REPO=ko.local/sandbox
 
 ko build --bare --tags=latest ./cmd/sandbox
